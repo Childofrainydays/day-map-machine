fc855c2851f71066c36363ebb1d75be8efd35bc5
Childofrainydays <107083153+Childofrainydays@users.noreply.github.com>
2023-07-27 20:14:30 -0600

    code correction on date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 // "$(document).ready(function() {...} code is the preferred way to write jQuery document ready event handlers." So sayeth Google.
 $(document).ready(function() {
 
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  $(".saveBtn").on("click", function() {
+    // Get the user input (description) from the corresponding textarea element.
+    // $(this) is the selector for the save button that was clicked. Resource: https://www.w3schools.com/jquery/jquery_ref_selectors.asp
+    const noteInput = $(this).siblings("textarea").val();
+
+     // Get the id of the containing time-block.
+     // .closest() method gets the first ancestor of the selected element. Resource: https://www.w3schools.com/jquery/traversing_closest.asp
+     const timeBlockId = $(this).closest(".time-block").attr("id");
   
+    // Save the user input in local storage using the time-block id as the key.
+    // .setItem() method sets the value of the specified local storage item. Resource: https://www.w3schools.com/jsref/met_storage_setitem.asp
+    localStorage.setItem(timeBlockId, description);
+  });
 
     // Function to update classes for the time blocks
     function updateTimeBlocks() {
@@ -32,12 +34,13 @@ $(document).ready(function() {
         // Add appropriate class based on comparison. Resource: https://www.w3schools.com/jquery/html_addclass.asp
         // Needs to  iterate through three possible classes, so used if; else if; else 
         if (blockHour < currentHour) {
-          $(this).addClass("past");
+          $(this).addClass("past"); // time-block is in the past
         // === is the strict equality operator. Resource: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Comparison_Operators
-        } else if (blockHour === currentHour) {
-          $(this).addClass("present");
+        } else if (blockHour === currentHour) { 
+          // .addClass() method adds one or more class names to the selected elements. Resource: https://www.w3schools.com/jquery/html_addclass.asp
+          $(this).addClass("present"); // time-block is in the present
         } else {
-          $(this).addClass("future");
+          $(this).addClass("future"); // time-block is in the future
         }
       });
     }
@@ -57,18 +60,16 @@ $(document).ready(function() {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   
-  // Function to update the date display
-  function updateDate() {
+// Function to update the date display
+function updateDate() {
   const currentDate = new Date();
   // Format the date to be displayed on the page. Resource: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   const formattedDate = currentDate.toLocaleDateString(undefined, options);
-  // Set the text content of the element with id="currentDay" to the formatted date
+  // Set the text content of the element with id="currentDate" to the formatted date
   document.getElementById('currentDate').textContent = formattedDate;
 }
 
 // Call the function to update the date on page load
 updateDate();
 });
-
-
